b084cc018438b7c6bb73633ea38e9d58133a42ba
LACHKAR99ZAKARIA <lachkar99zaakria@gmail.com>
2023-08-11 11:33:39 +0100

    init files

--- Package.swift
+++ Package.swift
@@ -21,8 +21,5 @@ let package = Package(
         .target(
             name: "MapSwifty",
             dependencies: []),
-        .testTarget(
-            name: "MapSwiftyTests",
-            dependencies: ["MapSwifty"]),
     ]
 )
--- Sources/MapSwifty/MapSwifty.swift
+++ Sources/MapSwifty/MapSwifty.swift
@@ -1,6 +1,46 @@
-public struct MapSwifty {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
+import MapKit
 
-    public init() {
+public struct MapSwifty: UIViewRepresentable {
+    let mapView = MKMapView()
+
+    // MARK: Helpers
+    public func makeUIView(context: Context) -> MKMapView {
+        mapView.delegate = context.coordinator
+        mapView.isRotateEnabled = false
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+//        let zoomRange = MKMapView.CameraZoomRange(minCenterCoordinateDistance: 500, maxCenterCoordinateDistance: 20000)
+//        mapView.cameraZoomRange = zoomRange
+        mapView.mapType = .standard
+        return mapView
+    }
+
+    public func updateUIView(_ uiView: MKMapView, context: Context) {
+    }
+
+    public func makeCoordinator() -> MapCoordinator {
+        return MapCoordinator(parent: self)
+    }
+}
+
+extension MapSwifty {
+    public class MapCoordinator: NSObject, MKMapViewDelegate {
+        let parent : MapSwifty
+        var userLocationCoordinate : CLLocationCoordinate2D?
+        var currentRegion: MKCoordinateRegion?
+        var previousSelectedIndex: Int?
+        public init(parent: MapSwifty) {
+            self.parent = parent
+        }
+        // MARK: Helpers
+        // MARK: didUpdate userLocation
+        public func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        }
+        // MARK: didUpdate region
+        public func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        }
+        public func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+        }
     }
 }
