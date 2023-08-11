8c94a6e9c7b5ff1862e0859acfb0ecb9a1550fcb
LACHKAR99ZAKARIA <lachkar99zaakria@gmail.com>
2023-08-11 13:11:36 +0100

    init

--- Sources/MapSwifty/MapSwifty.swift
+++ Sources/MapSwifty/MapSwifty.swift
@@ -1,46 +1,46 @@
 import SwiftUI
 import MapKit
 
-struct MapSwifty: UIViewRepresentable {
+public struct MapSwifty: UIViewRepresentable {
     let mapView = MKMapView()
 
     // MARK: Helpers
-    func makeUIView(context: Context) -> MKMapView {
+    public func makeUIView(context: Context) -> MKMapView {
         mapView.delegate = context.coordinator
         mapView.isRotateEnabled = false
         mapView.showsUserLocation = true
         mapView.userTrackingMode = .follow
-//        let zoomRange = MKMapView.CameraZoomRange(minCenterCoordinateDistance: 500, maxCenterCoordinateDistance: 20000)
-//        mapView.cameraZoomRange = zoomRange
+        // let zoomRange = MKMapView.CameraZoomRange(minCenterCoordinateDistance: 500, maxCenterCoordinateDistance: 20000)
+        // mapView.cameraZoomRange = zoomRange
         mapView.mapType = .standard
         return mapView
     }
 
-    func updateUIView(_ uiView: MKMapView, context: Context) {
+    public func updateUIView(_ uiView: MKMapView, context: Context) {
     }
 
-    func makeCoordinator() -> MapCoordinator {
+    public func makeCoordinator() -> MapCoordinator {
         return MapCoordinator(parent: self)
     }
 }
 
 extension MapSwifty {
-    class MapCoordinator: NSObject, MKMapViewDelegate {
+    public class MapCoordinator: NSObject, MKMapViewDelegate {
         let parent : MapSwifty
         var userLocationCoordinate : CLLocationCoordinate2D?
         var currentRegion: MKCoordinateRegion?
         var previousSelectedIndex: Int?
-        init(parent: MapSwifty) {
+        public init(parent: MapSwifty) {
             self.parent = parent
         }
         // MARK: Helpers
         // MARK: didUpdate userLocation
-        func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        public func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
         }
         // MARK: didUpdate region
-        func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        public func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
         }
-        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+        public func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
         }
     }
 }
